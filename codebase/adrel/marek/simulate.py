d73e331ff29f488889c19c976edc3781b11cb440
Shayan Amani <7shayan7@gmail.com>
2019-04-29 13:48:35 -0400

    q values policy adapted

--- codebase/adrel/marek/simulate.py
+++ codebase/adrel/marek/simulate.py
@@ -5,13 +5,24 @@ import numpy as np
 import pandas as pd
 import tqdm
 
-# number of runs to determine how good is the policy
-trials = 2
+import matplotlib.pyplot as plt
+
+
+def select_action(s):
+    state_q = q_values.loc[q_values['idstate'] == s]
+    a = state_q.max()[1]
+    return int(a)
+
 
 env = gym.make('CartPole-v1')
-policy = pd.read_csv("policy_nn.csv")
-# converting to an nparray
-scales = pd.read_csv("scales.csv").values
+
+# policy = pd.read_csv("initial/policy_nn.csv")
+policy = pd.read_csv("qvals/policy_nn.csv")
+
+q_values = pd.read_csv("qvals/qvalues.csv")
+
+# scales = pd.read_csv("initial/scales.csv").values # converting to an ndarray
+scales = pd.read_csv("qvals/scales.csv").values  # converting to an ndarray
 
 # policy = pd.read_csv("codebase/adrel/marek/policy_nn.csv")
 # scales = pd.read_csv("codebase/adrel/marek/scales.csv").values
@@ -30,10 +41,14 @@ else:
     policy_randomized = False
     probabilities = None
 
-totalreward = 0
+# number of runs to determine how good is the policy
+trials = 100
+is_q = False
+total_reward = 0
 
 # n = 0
 # m = 0
+rewards = dict()
 info_list = []
 
 # will not include sleep time as time.perf_counter will
@@ -51,7 +66,7 @@ for trial in tqdm.trange(trials):
         if i > 0:
             # find the closest state
             statescaled = state @ scales
-            statefeatures += eps * state
+            # statefeatures += eps * state
             dst = np.linalg.norm(statefeatures - np.repeat(np.atleast_2d(statescaled), statefeatures.shape[0], 0),
                                  axis=1)
 
@@ -67,7 +82,8 @@ for trial in tqdm.trange(trials):
                 # m += 1
             else:
                 # assume that there is a single action for each state
-                action = int(actions[statei])
+                # switch between q-learning or the policy
+                action = select_action(statei) if is_q else int(actions[statei])
 
             print(i, stateids[statei], action, values[statei], np.linalg.norm(statescaled - statefeatures[statei]))
         else:
@@ -82,22 +98,37 @@ for trial in tqdm.trange(trials):
         print(state, reward, done, info)
         env.render()
 
-
         # (state, reward, done, info) = en v.step(action)  # take a random action
         if any(info):
             info_list.append(info)
 
-        totalreward += reward
+        if trial in rewards:
+            rewards[trial] += reward
+        else:
+            rewards[trial] = reward
+
+        total_reward += reward
 
 env.close()
 
 elapsed_time = time.process_time() - t_elapsed
 perf_time = time.perf_counter() - t_perf
 
-print("Average reward per trial", totalreward / trials)
+print("Average reward per trial", total_reward / trials)
 
 print("elapsed time:", elapsed_time)
 print("elapsed time:", perf_time)
 print('list of info is:', info_list)
+
+x_axis = np.array([i for i in range(trials)])
+y_axis = np.array([i for i in rewards.values()])
+
+cm = plt.cm.get_cmap('viridis')
+# cc = np.linspace(0, 2, 30)
+# sc = plt.scatter(x_axis, y_axis, vmin=0, vmax=200)
+plt.plot(x_axis, y_axis, '-')
+# plt.colorbar(sc)
+plt.title("Reward per each trial using the provided {}".format('q values' if is_q else 'policy'))
+plt.show()
 
 
