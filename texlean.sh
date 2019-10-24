2508fe537d34cbad2ed79262e4452da6ee7f9e6d
SHi-ON <7shayan7@gmail.com>
2019-10-24 14:16:03 -0400

    Latex trash Shell updated

--- texlean.sh
+++ texlean.sh
@@ -1,4 +1,5 @@
 # trashes the Latex generated peripheral files
 # Shayan Amani - Oct 2019
 
+trash ./out
 find . -type f -name '*.aux' -o -name '*.gz' -o -name '*.log' -o -name '*.out' | xargs -L1 trash
