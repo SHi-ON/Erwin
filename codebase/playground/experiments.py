f8ca2f2e70ceabed869ca3b7a71c491c6921284a
SHi-ON <7shayan7@gmail.com>
2020-02-10 14:01:22 -0500

    machine replacement on cube

---
+++ codebase/playground/experiments.py
@@ -0,0 +1,14 @@
+
+if 4 % 2:
+    print("final way of the trueness")
+
+if 3 % 2:
+    print("should be printed!")
+
+a = [u for u in range(9)]
+
+l = len(a) // 2 if 4 % 2 else len(a) // 2 + 1
+print(l)
+
+if -1:
+    print("False")
